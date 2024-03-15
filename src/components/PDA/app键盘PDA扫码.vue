d1661f5ff8ec3b8a0b3bf2a01b1db31921667b96
孟磊 <1598747809@qq.com>
2024-03-15 11:26:28 +0800

    解决键盘扫码PDA的离开页面在返回无焦点，解决输入框数据每次扫码会拼接之前的数据的问题

--- src/components/PDA/app键盘PDA扫码.vue
+++ src/components/PDA/app键盘PDA扫码.vue
@@ -1,13 +1,21 @@
 <template>
   <!-- 扫码input -->
-  <input
-    :value="inputValue"
+  <!-- style="opacity: 0;
+  position: absolute" 这个样式可以隐藏元素，但功能还在 -->
+  <!-- 外层还有个盒子的时候   点击别的地方也会使输入框失去焦点 绑定失去焦点事件会在获取焦点-->
+  <div @tap="setfocus()">
+    <input
+    v-if="flag"
+    v-model="pvalue"
     @focus="focus"
     @blur="setfocus"
+    @input="bindputFocus"
     :focus="focusType"
     @confirm="confirm"
     placeholder="请输入"
   />
+  </div>
+ 
 </template>
 
 <script>
@@ -15,7 +23,8 @@ export default {
   name: "",
   data() {
     return {
-      inputValue:'',
+      flag:true, //控制每次进来时dom元素的显示，防止跳转别的页面之后，回来这个页面焦点就失去了
+      pvalue:'',//双向绑定，为了清空输入框值
       focusType: true, //控制输入框聚焦  初始必须是true
     };
   },
@@ -44,9 +53,14 @@ export default {
         this.focusType = true;
       });
     },
+    // 在每次input输入的时候清空之前的内容，防止每次扫码之前的内容还在
+    bindputFocus(){
+      this.$nextTick(()=>{
+        this.pvalue = '' //每次清空输入框的内容
+      })
+    },
     // 聚焦时
     focus() {
-      this.inputValue = "";
       console.log("输入框聚焦");
       // 并不能完全禁用软键盘
       setTimeout(function () {
@@ -56,15 +70,40 @@ export default {
     },
     // 回车
     confirm: function (e) {
-      this.inputValue = "";
       console.log(e.detail.value, "回显数据----");
     //e.detail.value   {"code":"S-B-A1-90107123 共享办公桌 A1 1600*800"}
       let proxy = e.detail.value.substring(0, e.detail.value.indexOf(" "));
       console.log(proxy, "&&&&&&&&&&");//S-B-A1-90107123
 
      //拿到参数后进行后续操作
-    
+
     },
+
+    // 注意  在小程序或者app中  从这个当前页跳转到别的页面，在退回来的时候，onload生命周期不会被再次加载,导致输入框不饿能默认有焦点，没有焦点，扫码枪就扫不到内容回显
+    
+    // 现在要跳转了
+    // this.flag = false //跳转页面之前隐藏dom
+    // uni.navigateTo({
+		// 			url: url,
+		// 			events: { //需要在这里监听跳转过去的页面在退回来时发射的事件
+		// 				refreshPage: () => { //refreshPage  为事件名称
+		// 					// 刷新dom
+		// 					this.flag = true;//显示dom
+		// 				},
+		// 			},
+		// 		})
+
+
+    //比如我现在在跳转过后的页面
+    // onLoad() {
+		//  this.eventChannel = this.getOpenerEventChannel();//ml
+    // },
+    // //点击返回上一页
+    // uni.navigateBack({
+    //     delta: 1,
+    //   });
+	  //   // 刷新上一页面
+	  // this.eventChannel.emit("refreshPage");// 向上一页发射个方法
   },
 };
 </script>
