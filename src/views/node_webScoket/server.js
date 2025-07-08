3bada86a2d34796daeaea09e381e1ab919ed2187
孟磊 <1598747809@qq.com>
2025-07-08 22:29:23 +0800

    优化服务

--- src/views/node_webScoket/server.js
+++ src/views/node_webScoket/server.js
@@ -5,15 +5,14 @@ const WebSocket = require('ws');
 const app = express();
 const server = http.createServer(app);
 
-const port = 3001;
+const port = 3002;
 
 //创建WebSocketconst服务
 wss = new WebSocket.Server({ port }, () => {
   console.log('服务开启');
 });
 
-// 建立连接
-wss.on('connection', connectHandle)
+
 
 const connectHandle = (ws) => {
   console.log('客户端连接了');
@@ -33,10 +32,24 @@ const closeHandle = (e) => {
   console.log('closeHandle=======>客户端断开了 ', e);
 }
 const messageHandle = (data) => {
-  console.log('message =======>  ', JSON.parse(data));
+  console.log('message =======>  ', data);
 }
 
 
-server.listen(port, () => {
-  console.log(`WebSocket服务器运行在 ${port} 端口`);
+// 建立连接
+wss.on('connection', connectHandle)
+
+
+
+server.listen(8888, () => {
+  console.log(`WebSocket服务器运行在 ${8888} 端口`);
+});
+
+// 捕获启动错误（如端口被占用）
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`⚠️ 端口 ${port} 已被占用，请更换端口`);
+  } else {
+    console.error('服务器发生错误:', err);
+  }
 });
